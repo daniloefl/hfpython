3166e1757e500a9a0fafa035ced47dea03907b64
Danilo Ferreira de Lima <daniloefl@gmail.com>
2016-06-02 01:01:33 +0200

    Update

--- hydrogen_gauss.py
+++ hydrogen_gauss.py
@@ -5,145 +5,124 @@ import matplotlib.pyplot as plt
 # to solve
 # - hbar^2 / (2 m) deriv(deriv(xi)) + [ V + ( hbar^2 l (l+1) ) / (2 m r^2) - E ] xi (r) = 0
 # xi = r*R(r), where R(r) is the radial solution
-# deriv(deriv(xi)) + [ 2 m (E - V) - l (l+1)/r^2 ] xi = 0
-# xi = sum_i c_i 1/sqrt(4 pi s_i^2) exp(-r^2/(2 s_i^2)) = sum_i c_i f_i
-# deriv(xi) = - sum_i c_i s_i^2/sqrt(4 pi s_i^2) r exp(-r^2/(2 s_i^2))
-# deriv(deriv(xi)) = sum_i c_i s_i^2/sqrt(4 pi s_i^2) (s_i^2 r - 1) exp(-r^2/(2 s_i^2))
-#                  = sum_i c_i f_i (s_i^2 (s_i^2 r - 1) )
-# sum_i c_i [ s_i^2 (s_i^2 r - 1) + 2 m (E - V) - l (l+1)/r^2 ] f_i = 0
-# multiply by xi and integrate in r
-# int sum_ij c_i c_j f_i f_j [ s_i^2 ( s_i^2 r - 1) ] dr = sum_ij c_i c_j {
-# int f_i f_j dr = int 1/(4 pi s_i^2 s_j^2) exp(-r^2/(1/(2s_i^2) + 1/(2s_j^2))) dr = 
-# int f_i f_j r dr = int r 1/(4 pi s_i^2 s_j^2) exp(-r^2/(1/(2s_i^2) + 1/(2s_j^2))) dr = 
+# - r^2/2m deriv(deriv(xi)) - [ V - l (l+1)/r^2 ] r^2 xi = E r^2 xi
+# xi = sum_i c_i exp(-a_i r^2) = sum_i c_i f_i
+
+# deriv(xi) = - sum_i c_i a_i r exp(-a_i r^2)
+
+# deriv(deriv(xi)) = sum_i c_i a_i (a_i r - 1) exp(-a_i r^2)
+#                  = sum_i c_i f_i a_i (a_i r - 1)
+
+# int_0^inf exp(-a x^2) dx = 0.5 (pi/a)^0.5
+# int_0^inf x exp(-a x^2) dx = 1/(2a)
+# int_0^inf x^2 exp(-a x^2) dx = 1!! sqrt(pi)/(4 a^1.5) 
+# int_0^inf x^3 exp(-a x^2) dx = 1/(2 a^2)
+# int_0^inf x^4 exp(-a x^2) dx = 3!! sqrt(pi) / ( 8 a^2.5)
+# int_0^inf x^5 exp(-a x^2) dx = 2 / ( 2 a^3 )
+# int f_i f_j dr = int exp(-(a_i+a_j) r^2) dr = 0.5 (pi/(a_i+a_j))^0.5
+
+# - 1/2m int r^2 xi deriv(deriv(xi)) dr = -1/2m sum_ij c_i c_j int a_i (a_i r^3 - r^2) exp (- (a_i + a_j) r^2) dr
+#                                       = -1/2m sum_ij c_i c_j { a_i^2 / [ 2 (a_i + a_j) ] - a_i sqrt(pi) / [ 4 (a_i + a_j)^1.5 ] }
+#
+# + int xi^2 Z r dr = Z sum_ij c_i c_j int exp(-(a_i + a_j) r^2) r dr = Z sum_ij c_i c_j / [ 2 (a_i + a_j) ]
+#
+# + l (l+1) int xi^2 dr = l (l+1)/2 sum_ij c_i c_j sqrt[pi/(a_i+a_j)]
+#
+# int r^2 xi^2 dr = sum_ij c_i c_j sqrt(pi)/[ 4 (a_i + a_j)^1.5 ]
+#
+# sum_i { -1/2 { a_i^2 / [ 2 (a_i + a_j) ] - a_i sqrt(pi) / [ 4 (a_i + a_j)^1.5 ] } + Z / [ 2 (a_i + a_j) ] + l (l+1)/2 sqrt[pi/(a_i+a_j)] } c_i = 0
+#
+#
+
+def init(N = 4, dx = 0.01):
+    a = np.zeros(N)
+    for i in range(0, N):
+        a[i] = dx*i +dx
+    return a
+
 
 # Coulomb potential
-def V(r, Z):
-    pot = np.zeros(len(r))
-    for i in range(0, len(r)):
-        if r[i] != 0:
-            pot[i] = -Z/r[i]
-	else:
-	    pot[i] = pot[i-1]
-    return pot
+def Coulomb(Z, a):
+    V = np.zeros((len(a), len(a)))
+    for i in range(0, len(a)):
+        for j in range(0, len(a)):
+	    V[i, j] = Z/(2*(a[i] + a[j]))
+    return V
+
+# l
+def Vcent(l, a):
+    V = np.zeros((len(a), len(a)))
+    for i in range(0, len(a)):
+        for j in range(0, len(a)):
+	    V[i, j] = l*(l+1)/2.0*np.sqrt(np.pi/(a[i] + a[j]))
+    return V
+
+# S
+def overlap(a):
+    V = np.zeros((len(a), len(a)))
+    for i in range(0, len(a)):
+        for j in range(0, len(a)):
+	    V[i, j] = np.sqrt(np.pi)/(4*(a[i] + a[j])**1.5)
+    return V
+
+# Kinetic energy
+def kinetic(a):
+    V = np.zeros((len(a), len(a)))
+    for i in range(0, len(a)):
+        for j in range(0, len(a)):
+	    V[i, j] = -0.5*(a[i]**2/(2*(a[i] + a[j])) - a[i]*np.sqrt(np.pi)/(4*(a[i] + a[j])**1.5))
+    return V
+
+N = 8
+dx = 0.02
+a = init(N, dx)
+M = kinetic(a) + Vcent(l = 0, a = a) + Coulomb(Z = 1, a = a)
+S = overlap(a)
+Sinv = np.linalg.inv(S)
+w, v = np.linalg.eig(np.matmul(Sinv, M))
 
 # initialise Grid
-def init(dx, N, xmin):
+def initGrid(dx, N, xmin):
     r = np.zeros(N)
     for i in range(0, N):
         r[i] = np.exp(xmin + i*dx)
     return r
 
-# y is the rescaled wave function
-# y = 1/(sqrt(r)) * xi
-# xi = r*R(r), where R(r) is the radial solution
-# the xi -> y transformation prevents a first derivative from appearing in the Schroedinger eq.
-
-# a and f re the temporary variables in the Numerov method
-# we want to solve deriv(deriv(y)) + [2m_e/hbar^2 r^2 (E - V)  - (l+0.5)^2 ] y = 0
-# using Numerov's method:
-# a = 2 m / hbar^2 * r^2 * (E - V)  - (l+0.5)^2
-# f = 1 + a * dx^2/12
-# y_{n+1} = ((12 - 10 f_n) y_n - f_{n-1} y_{n-1}) / f_{n+1}
-# the derivative above is a function of x, where x = log(r)
-# The original equation is:
-# - hbar^2 / (2 m) deriv(deriv(xi)) + [ V + ( hbar^2 l (l+1) ) / (2 m r^2) - E ] xi (r) = 0
-# where in the latter eq., the derivative is in relation to r.
-# One other way of doing this is to keep the r(x) logarithm grid in WF, and try to solve
-# the eq. in terms of r, so it would be:
-# deriv(deriv(xi)) + 2 m / hbar^2 [ E - V - (hbar^2 l (l+1) ) / 2 m r^2 ] xi (r) = 0
-# in which case, a = 2 m / hbar^2 [ E - V - (hbar^2 l (l+1) ) / 2 m r^2 ] and dx is replaced by r(i) - r(i-1)
-# the problem is that then there is a 1/r dependenci in a(r), which causes instability
-def solve(r, dx, pot, n, l, E, Z):
-    m = 1.0
-    a = np.zeros(len(r))
-    f = np.zeros(len(r))
-    y = np.zeros(len(r))
-    yp = np.zeros(len(r))
-    icl = -1
-    no = 0
-    nop = 0
-    for i in range(0, len(r)):
-        a[i] = 2*m*r[i]**2*(E - pot[i]) - (l+0.5)**2
-	f[i] = 1 + a[i]*dx**2/12.0
-	if icl < 0 and i >= 1 and a[i]*a[i-1] < 0:
-	    icl = i
-    
-    y[0] = ((Z*r[0])**(l+0.5))/n
-    y[1] = ((Z*r[1])**(l+0.5))/n
-    #y[0] = 2*(r[0]**(l+1))*(1 - Z*r[0]/(2*l+2))/np.sqrt(r[0])
-    #y[1] = 2*(r[1]**(l+1))*(1 - Z*r[1]/(2*l+2))/np.sqrt(r[1])
-    for i in range(1, len(r)-1):
-	y[i+1] = ((12 - f[i]*10)*y[i] - f[i-1]*y[i-1])/f[i+1];
-	if y[i]*y[i+1] < 0:
-	    no += 1
-
-    yp[len(r)-1] = np.exp(-np.sqrt(-2*m*E)*r[len(r)-1])
-    yp[len(r)-2] = np.exp(-np.sqrt(-2*m*E)*r[len(r)-2])
-    #yp[len(r)-1] = y[len(r)-1]
-    #yp[len(r)-2] = ((12 - f[len(r)-1]*10)*yp[len(r)-1])/f[len(r)-2]
-    for i in reversed(range(1, len(r)-1)):
-	yp[i-1] = ((12 - f[i]*10)*yp[i] - f[i+1]*yp[i+1])/f[i-1];
-	#if yp[i-1] > 10:
-	#  for j in reversed(range(i-1, len(r)-1)):
-	#    yp[j] /= yp[i-1]
-
-    for i in reversed(range(1, len(r)-1)):
-	if yp[i-1]*yp[i] < 0:
-	    nop += 1
-    return [y, yp, icl, no, nop]
-
-def toPsi(x, y):
+def intoGrid(r, a, v):
+    xi = np.zeros(len(r))
+    for i in range(0, len(v)):
+        xi += v[i]*np.exp(-a[i]*r**2)
     n = 0
-    psi = np.zeros(len(y))
-    for i in range(0, len(y)):
-        psi[i] = y[i]*x[i]**(-0.5) # undo y->R(r) transform
-	ip = len(y)-2
-	if i < len(y)-1:
+    for k in range(0, len(r)):
+	ip = len(r)-2
+	if i < len(r)-1:
 	    ip = i + 1
 	dr = np.fabs(r[ip]-r[i])
-        n += (x[i]*psi[i])**2*dr        # normalise it so that int |r R(r)|^2 dr == 1
-    if n != 0:
-        for i in range(0, len(y)):
-            psi[i] /= np.sqrt(n)
-    return psi
+        n += r[k]*xi[k]**2*dr
+    coeff = 1.0
+    if xi[0] < 0:
+        coeff = -1.0
+    for k in range(0, len(r)):
+        xi[k] /= coeff*np.sqrt(n)
+    return xi
 
-def nodes(n, l):
-    return n - l - 1
-
-n = 1
-l = 0
-Z = 1
-E = -1
-Emax = 0.0
-Emin = -20.0
-dx = 1e-3
-r = init(dx, 14000, np.log(1e-4))
-pot = V(r, Z)
-no = 0
-nop = 0
-old_nodes = -1
-old_E = 0
-old_dE = 0
-prev_dE = 0
-while E < 0:
-    E = input('Guess a value for the correct energy [0 to exit]: ')
-    if E > 0:
-        break
-    [y, yp, icl, no, nop] = solve(r, dx, pot, n, l, E, Z)
-    print "Information: E = ", E, ", nodes = ", no, nop, ", expected nodes = ", nodes(n, l), ", crossing zero at = ", icl
-    psi = toPsi(r, y)
-    psip = toPsi(r, yp)
-    exact_p = 2*np.exp(-r) # exact R(r) solution for n = 1
-    idx = np.where(r > 5)
-    idx = idx[0][0]
-    plt.clf()
-    plt.plot(r[0:idx], psi[0:idx], 'r-', label='$R_{-}$')
-    plt.plot(r[0:idx], psip[0:idx], 'b-', label='$R_{+}$')
-    plt.plot(r[0:idx], exact_p[0:idx], 'g--', label='$R_{exact}$')
-    plt.legend(('$R(r)$ from 0', '$R(r)$ from +$\\infty$', 'Exact $R(r)$'), frameon=False)
-    plt.xlabel('$r$')
-    plt.ylabel('$|R(r)|$')
-    plt.title('')
-    plt.draw()
-    plt.show()
+r = initGrid(1e-3, 14000, np.log(1e-4))
+xi = []
+st = ['r-', 'r:', 'r--', 'r-.', 'b-', 'b:', 'b--', 'b-.']
+for j in range(0, N):
+    xi.append(intoGrid(r, a, v[:, j]))
+    print "Eigenvalue ", j , " energy ", w[j]
+    
+exact_p = 2*np.exp(-r) # exact R(r) solution for n = 1
+idx = np.where(r > 10)
+idx = idx[0][0]
+plt.clf()
+for j in range(0, N):
+    plt.plot(r[0:idx], xi[j][0:idx], st[j], label=str(j))
+plt.plot(r[0:idx], exact_p[0:idx], 'g--', label='$R_{exact}$')
+plt.xlabel('$r$')
+plt.ylabel('$|R(r)|$')
+plt.title('')
+plt.draw()
+plt.show()
 
